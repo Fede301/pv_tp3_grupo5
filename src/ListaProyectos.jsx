f2074042cd28da36c40a7f5f8dd03b09ba9cedd1
micaelsoruco332 <micaelsoruco332@gmail.com>
2026-05-09 20:21:46 -0300

    estructura listaproyectos.jsx finish

---
+++ src/ListaProyectos.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { obtenerProyectos,eliminarProyectos } from './services/proyectoService'; 
+import {obtenerProyectos,eliminarProyectos, buscarProyecto} from './services/proyectoService';
+const ListaProyectos = () => {
+const [proyectos, setProyectos] = useState(obtenerProyectos());
+const handleElimiminar=(id)=>{
+    eliminarProyectos(id);
+    
+}
+const listaActualizada=obtenerProyectos();
+setProyectos(listaActualizada);
+const handleBuscar=(evento)=>{
+    const texto=evento.target.value;
+    const resultado=buscarProyecto(texto);
+    setProyectos(resultado);
+}
+    return (
+        <div>
+            <h2>Lista de Proyectos</h2>
+            <input type="text" placeholder="Buscar proyecto por título " onChange={handleBuscar} />
+            <div className="proyectos-grid">
+                {proyectos.map((proyecto) => (
+                    <div key={proyecto.id}>
+                        <h3>{proyecto.titulo}</h3>
+                        <p>Categoría: {proyecto.categoria}</p>
+                        <p>Estado: {proyecto.estado}</p>
+                        <button onClick={() => handleElimiminar(proyecto.id)}>Eliminar</button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ListaProyectos;
