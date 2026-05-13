3b938f05a2eac33abf5e7ee8541445166c2c0406
Federico <federicofof305@gmail.com>
2026-05-13 18:25:57 -0300

    Solucion de conflictos

--- src/components/ListaProyectos.jsx
+++ src/components/ListaProyectos.jsx
@@ -1,32 +1,25 @@
 import '../css/stylesproyectos.css';
 import { useState } from 'react';
-<<<<<<< HEAD:src/ListaProyectos.jsx
-import proyectoService, { obtenerProyectos, eliminarProyectos, buscarproyecto } from './services/proyectoService';
-=======
 import { obtenerProyectos,eliminarProyectos,buscarProyecto} from '../services/proyectoService';
 import ProyectoCard from './ProyectoCard';
->>>>>>> 15b7cfa83b3e6fd5d270ba0287b23cd96107e563:src/components/ListaProyectos.jsx
 const ListaProyectos = () => {
     const [proyectos, setProyectos] = useState(obtenerProyectos());
-    const [busqueda, setbusqueda] = useState("");
-    const handleElimiminar = (id) => {
+    const [busqueda, setBusqueda] = useState("");
+    
+    const handleElimiminar=(id)=>{
         eliminarProyectos(id);
-        const listaActualizada = obtenerProyectos();
+        const listaActualizada=obtenerProyectos();
         setProyectos(listaActualizada);
     }
-    const handlebuscar = (e) => {
-        const valor = (e.target.value);
-        setbusqueda(valor);
-        setProyectos(proyectoService.buscarproyecto(valor));
-    };
+    const handleBuscar=(evento)=>{
+        setBusqueda(evento.target.value);
+        const resultado=buscarProyecto(evento.target.value);
+        setProyectos(resultado);
+    }
     return (
         <div>
             <h2>Lista de Proyectos</h2>
-            <input type="text"
-                placeholder="Buscar proyecto por título "
-                value={busqueda}
-                onChange={handlebuscar}
-            />
+            <input type="text" placeholder="Buscar proyecto por título " value={busqueda} onChange={handleBuscar} />
             <div className="proyectos-grid">
                 {proyectos.map((proyecto, id) => (
                     <ProyectoCard key={id} datosProyecto={proyecto} handleElimiminar={handleElimiminar}/>
--- src/services/proyectoService.js
+++ src/services/proyectoService.js
@@ -1,24 +1,21 @@
-const proyectoService = (() => {
+const proyectoService=(()=>{
     let proyectos = [
-        { id: 1, titulo: "Proyecto ", estado: "Finalizado", categoria: "Educacion" },
-        { id: 2, titulo: "Proyecto 2", estado: "En proceso", categoria: "Salud" },
-        { id: 3, titulo: "Proyecto 3", estado: "Finalizado", categoria: "Tecnologia" },
-        { id: 4, titulo: "Proyecto 4", estado: "En proceso", categoria: "Educacion" },
+        {id:1, titulo:"Proyecto 1", estado:"Finalizado", categoria:"Educacion"},
+        {id:2, titulo:"Proyecto 2", estado:"En proceso", categoria:"Salud"},
+        {id:3, titulo:"Proyecto 3", estado:"Finalizado", categoria:"Tecnologia"},
+        {id:4, titulo:"Proyecto 4", estado:"En proceso", categoria:"Educacion"},
     ];
 
     const obtenerProyectos = () => [...proyectos];
     const agregarProyectos = (infoProyecto) => proyectos.push(infoProyecto);
     const eliminarProyectos = (idEliminar) => {
-        proyectos = proyectos.filter(proyecto => proyecto.id !== idEliminar);
+        proyectos=proyectos.filter(proyecto => proyecto.id !== idEliminar);
     }
-    const buscarproyecto = (valor) => {
-        return proyectos.filter((p) => p.titulo.toLowerCase().includes(valor.toLowerCase())
-
-        )
-    }
-    return { obtenerProyectos, agregarProyectos, eliminarProyectos, buscarproyecto };
+    const buscarProyecto = (textoBuscado) =>
+        proyectos.filter(proyecto => proyecto.titulo.toLowerCase() === textoBuscado.toLowerCase());
+    return {obtenerProyectos, agregarProyectos, eliminarProyectos, buscarProyecto};
 })();
 
 export default proyectoService;
 
-export const { obtenerProyectos, agregarProyectos, eliminarProyectos, buscarproyecto } = proyectoService;
+export const {obtenerProyectos, agregarProyectos, eliminarProyectos, buscarProyecto} = proyectoService;
