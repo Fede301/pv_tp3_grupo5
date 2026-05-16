cce07cf52c3923e2faf138d8c1e5387922a7dd9d
AlancayJuan <juanalancay.mv@gmail.com>
2026-05-16 02:11:37 -0300

    Agrego DetalleProyecto, su boton en proyectoCard y agrego mas informacion a los Proyectos

---
+++ src/components/DetalleProyecto.jsx
@@ -0,0 +1,32 @@
+const DetalleProyecto = ({ proyecto }) => {
+
+    if (!proyecto) {
+        return <p>Seleccione un proyecto</p>;
+    }
+
+    const { titulo, descripcion, recursos, equipo} = proyecto;
+    
+    return (
+        <section>
+            <h2>{titulo}</h2>
+            {descripcion.map((texto, index) => (
+                <p key={index}>{texto}</p>
+            ))}
+            
+            <h3>Recursos</h3>
+            <ul>
+                <li><a href={recursos.pdf}>PDF</a></li>
+            </ul>
+
+            <h3>Equipo</h3>
+            <ul>
+                {equipo.map((persona, index) => (
+                    <li key={index}>
+                        {persona.nombre} - {persona.rol}
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+};
+export default DetalleProyecto;
--- src/services/proyectoService.js
+++ src/services/proyectoService.js
@@ -1,9 +1,49 @@
 const proyectoService=(()=>{
     let proyectos = [
-        {id:1, titulo:"Proyecto 1", estado:"Finalizado", categoria:"Educacion"},
-        {id:2, titulo:"Proyecto 2", estado:"En proceso", categoria:"Salud"},
-        {id:3, titulo:"Proyecto 3", estado:"Finalizado", categoria:"Tecnologia"},
-        {id:4, titulo:"Proyecto 4", estado:"En proceso", categoria:"Educacion"},
+        {   id:1, titulo:"Proyecto 1", estado:"Finalizado", categoria:"Educacion",
+            descripcion: [
+                "Este proyecto busca Aplicar conocimientos prácticos, fomentar el trabajo en equipo y desarrollar habilidades específicas del área.",
+                "Realizamos experimentos para comprender fenómenos como la energía, el movimiento y la electricidad."
+            ],
+            recursos: { pdf: "/recursos/Taller de Física.pdf"},
+            equipo: [
+                {   nombre: "Juan Pérez", rol: "Docente de Física"},
+                {   nombre: "Ana Gómez", rol: "Asistente"}
+            ]
+        },
+        {   id:2, titulo:"Proyecto 2", estado:"En proceso", categoria:"Salud",
+            descripcion: [
+                "Este proyecto busca Aplicar conocimientos prácticos, fomentar el trabajo en equipo y desarrollar habilidades específicas del área.",
+                "Exploramos distintas técnicas artísticas como acuarela y acrílico, fomentando la creatividad y la expresión personal."
+            ],
+            recursos: { pdf: "/recursos/Taller de Pintura.pdf"},
+            equipo: [
+                {   nombre: "Ana Gómez", rol: "Docente de Arte"},
+                {   nombre: "Lucas Sivila", rol: "Asistente"}
+            ]
+        },
+        {   id:3, titulo:"Proyecto 3", estado:"Finalizado", categoria:"Tecnologia",
+            descripcion: [
+                "Este proyecto busca Aplicar conocimientos prácticos, fomentar el trabajo en equipo y desarrollar habilidades específicas del área.",
+                "En este taller desarrollamos proyectos utilizando componentes electrónicos y programación básica para construir y controlar robots."
+            ],
+            recursos: { pdf: "/recursos/Taller de Robótica.pdf"},
+            equipo: [
+                {   nombre: "Lucas Sivila", rol: "Docente de Robotica"},
+                {   nombre: "Maria Juarez", rol: "Asistente"}
+            ]
+        },
+        {   id:4, titulo:"Proyecto 4", estado:"En proceso", categoria:"Educacion",
+            descripcion: [
+                "Este proyecto busca Aplicar conocimientos prácticos, fomentar el trabajo en equipo y desarrollar habilidades específicas del área.",
+                "Creamos y producimos programas radiales, desarrollando habilidades de comunicación y trabajo en equipo."
+            ],
+            recursos: { pdf: "/recursos/Taller de Radio Escolar.pdf"},
+            equipo: [
+                {   nombre: "Maria Juarez", rol: "Docente de Comunicacion"},
+                {   nombre: "Juan Pérez", rol: "Asistente"}
+            ]
+        },
     ];
 
     const obtenerProyectos = () => [...proyectos];
