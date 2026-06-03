c0c69db09ea515248346e3248d484b5cc5e5385f
santtiago8 <46324472@fi.unju.edu.ar>
2026-06-03 16:02:24 -0300

    Agrego las rutas de navegacion

--- src/components/DetalleProyecto.jsx
+++ src/components/DetalleProyecto.jsx
@@ -1,18 +1,22 @@
+import { useParams } from "react-router-dom";
+import proyectoService from "../services/proyectoService";
+
 const DetalleProyecto = ({ proyecto }) => {
 
     if (!proyecto) {
         return <p>Seleccione un proyecto</p>;
     }
 
-    const { titulo, descripcion, recursos, equipo} = proyecto;
-    
+    const { titulo, descripcion, recursos, equipo } = proyecto;
+
+
     return (
         <div className="detalle-proyecto">
             <h2>{titulo}</h2>
             {descripcion.map((texto, index) => (
                 <p key={index}>{texto}</p>
             ))}
-            
+
             <h3>Recursos</h3>
             <ul>
                 <li><a href={recursos.pdf}>PDF</a></li>
--- src/components/ProyectoCard.jsx
+++ src/components/ProyectoCard.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const ProyectoCard = ({datosProyecto, handleElimiminar, setProyectoSeleccionado}) => {
+const ProyectoCard = ({ datosProyecto, handleElimiminar, setProyectoSeleccionado }) => {
   return (
     <div>
-        <h3>{datosProyecto.titulo}</h3>
-        <p>Categoría: {datosProyecto.categoria}</p>
-        <p>Estado: {datosProyecto.estado}</p>
-        <button onClick={() => handleElimiminar(datosProyecto.id)}>Eliminar</button>
-        <button onClick={() => setProyectoSeleccionado(datosProyecto)}>Ver detalle</button>
+      <h3>{datosProyecto.titulo}</h3>
+      <p>Categoría: {datosProyecto.categoria}</p>
+      <p>Estado: {datosProyecto.estado}</p>
+      <button onClick={() => handleElimiminar(datosProyecto.id)}>Eliminar</button>
+
+
+      { /* <button onClick={() => setProyectoSeleccionado(datosProyecto)}>Ver detalle</button> */}
+
+      <Link to={`/proyectos/${datosProyecto.id}`}>Ver detalle </Link>
+
     </div>
   )
 }
