94079cd4d9bd8397a32fadd8cb44f1c8560b5a10
Federico <federicofof305@gmail.com>
2026-06-13 23:39:24 -0300

    CSS sin uso

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import '../css/stylesindex.css';
 const Header = () => {
     return (
-        <header style={{ textAlign: 'center', padding: '20px' }}>
-            <h1 style={{ textDecoration: 'underline' }}>PROYECTOS EDUCATIVOS</h1>
+        <header>
+            <h1>PROYECTOS EDUCATIVOS</h1>
         </header>
 
     )
